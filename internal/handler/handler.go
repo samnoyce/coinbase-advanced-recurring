8e4babd009850140cea9824bb458964b6fb8aca3
Sam Noyce <samuel.noyce@outlook.com>
2026-01-13 22:54:40 -0600

    feat(handler): add order processing

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -12,6 +12,10 @@ type Handler struct {
 	logger *slog.Logger
 }
 
+type Event struct {
+	Orders []coinbase.Order `json:"orders"`
+}
+
 func New(client *coinbase.Client, logger *slog.Logger) *Handler {
 	return &Handler{
 		client: client,
@@ -19,6 +23,41 @@ func New(client *coinbase.Client, logger *slog.Logger) *Handler {
 	}
 }
 
-func (h *Handler) Run(ctx context.Context) error {
+func (h *Handler) Run(ctx context.Context, event Event) error {
+	for _, order := range event.Orders {
+		req := coinbase.NewCreateOrderRequest(&order)
+
+		resp, err := h.client.CreateOrder(ctx, req)
+		if err != nil {
+			h.logger.Error(
+				"Failed to create order",
+				"client_order_id", req.ClientOrderId,
+				"side", req.Side,
+				"product_id", req.ProductId,
+				"error", err,
+			)
+			continue
+		}
+
+		if !resp.Success {
+			h.logger.Error(
+				"Order rejected",
+				"client_order_id", req.ClientOrderId,
+				"side", req.Side,
+				"product_id", req.ProductId,
+				"error", resp.ErrorResponse.ErrorDetails,
+			)
+			continue
+		}
+
+		h.logger.Info(
+			"Order created",
+			"client_order_id", resp.SuccessResponse.ClientOrderId,
+			"side", resp.SuccessResponse.Side,
+			"product_id", resp.SuccessResponse.ProductId,
+			"order_id", resp.SuccessResponse.OrderId,
+		)
+	}
+
 	return nil
 }
